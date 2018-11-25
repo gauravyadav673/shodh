83afcb99f6db03efb78e7007c6f91fd8ad9be940
Mohit Yadav <mohityadav1921@gmail.com>
2018-11-25 18:35:39 +0530

    added flash messages

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', ensureAuthenticated, function(req, res, next) {
-  // req.user.admin = true;
- 	res.redirect('/users/login');
+router.get('/', ensureAuthenticated, function (req, res, next) {
+	// req.user.admin = true;
+	res.redirect('/users/login');
 });
 
 router.get('/addData', ensureAuthenticated, (req, res, next) => {
-   //req.user.admin = true;
+	//req.user.admin = true;
 	console.log(req.user.username);
-	res.render('addData', {title: 'Add Data', name: req.user.username});
+	res.render('addData', {
+		title: 'Add Data',
+		name: req.user.username
+	});
 });
 
 /*router.get('/profile', ensureAuthenticated, (req, res, next) => {
@@ -49,21 +52,25 @@ router.get('/addData', ensureAuthenticated, (req, res, next) => {
 });*/
 
 router.get('/admin', ensureAuthenticated, (req, res, next) => {
-  // req.user.admin = true;
-  res.render('admin', {'title': 'Admin Zone'});
+	// req.user.admin = true;
+	res.render('admin', {
+		'title': 'Admin Zone'
+	});
 });
 
 router.get('/yearwise', ensureAuthenticated, (req, res, next) => {
-  // req.user.admin = true;
-  res.render('yearwiseResults', {'title': 'Search By Year'});
+	// req.user.admin = true;
+	res.render('yearwiseResults', {
+		'title': 'Search By Year'
+	});
 });
 
 function ensureAuthenticated(req, res, next) {
 	//console.log(req.user);
-    if(req.isAuthenticated()){
-      return next();
-    }
-    res.redirect('/users/login');
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	res.redirect('/users/login');
 }
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var multer = require('multer');
-var upload = multer({dest:'./uploads'});
+var upload = multer({
+    dest: './uploads'
+});
 var User = require('../models/user')
 
 const rp = require('request-promise');
@@ -12,61 +14,63 @@ var citation = require('../models/userData').citation;
 /* GET users listing. */
 
 
-function retrieveCitations(scholarID, username){
+function retrieveCitations(scholarID, username) {
     var url = 'https://scholar.google.co.in/citations?user=' + scholarID;
     console.log(scholarID, username);
     rp(url)
-      .then(function(html){
-        var $ = cheerio.load(html);
-        //console.log(html);
-        var data = {};
-        var contDiv = $('#gsc_rsb_st');
-        var x=contDiv.children();
-        var y = x[1].children;
-        for(var i=0; i<y.length; i++){
-            data[y[i].children[0].children[0].children[0].data] = y[i].children[1].children[0].data;
-        }
-        //console.log(data);
-
-        var newCitation =  new citation({
-            username:username,
-            citaionCount:data["Citations"],
-            hIndex:data["h-index"],
-            iTenIndex:data["i10-index"]
-        });
-        newCitation.save(function(err, cit){
-            if(err){
-                console.log(err);
-                return 1;
+        .then(function (html) {
+            var $ = cheerio.load(html);
+            //console.log(html);
+            var data = {};
+            var contDiv = $('#gsc_rsb_st');
+            var x = contDiv.children();
+            var y = x[1].children;
+            for (var i = 0; i < y.length; i++) {
+                data[y[i].children[0].children[0].children[0].data] = y[i].children[1].children[0].data;
             }
-            return 0;
+            //console.log(data);
 
+            var newCitation = new citation({
+                username: username,
+                citaionCount: data["Citations"],
+                hIndex: data["h-index"],
+                iTenIndex: data["i10-index"]
+            });
+            newCitation.save(function (err, cit) {
+                if (err) {
+                    console.log(err);
+                    return 1;
+                }
+                return 0;
+
+            });
+
+        })
+        .catch(function (err) {
+            console.log(err);
+            return 1;
         });
-
-      })
-      .catch(function(err){
-        console.log(err);
-        return 1;
-      });
 }
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
 
-router.get('/register', function(req, res, next) {
-    res.render('register', {'title': 'Register'});
+router.get('/register', function (req, res, next) {
+    res.render('register', {
+        'title': 'Register'
+    });
 });
 
-router.post('/register', upload.single('profileimage'), function(req, res, next) {
+router.post('/register', upload.single('profileimage'), function (req, res, next) {
     var name = req.body.name;
     var email = req.body.email;
     var username = req.body.username;
     var scholarID = req.body.scholar_id;
     var password = req.body.password;
-    if(req.file){
+    if (req.file) {
         var profileimage = req.file.filename;
-    }else{
+    } else {
         var profileimage = 'noimage.jpg';
     }
     req.checkBody('name', 'Name field is required').notEmpty();
@@ -78,32 +82,32 @@ router.post('/register', upload.single('profileimage'), function(req, res, next)
     req.checkBody('password2', 'Password not matching').equals(req.body.password);
 
     var errors = req.validationErrors();
-    if(errors){
-        res.render('register',{
-            errors:errors
+    if (errors) {
+        res.render('register', {
+            errors: errors
         });
-    }else{
+    } else {
         console.log(name, email, username, scholarID, password);
-        User.getUserByUsername(username, function(err, user){
-            if(!user){
+        User.getUserByUsername(username, function (err, user) {
+            if (!user) {
                 var newUser = new User({
-                   name: name,
-                   email: email,
-                   username: username,
-                   scholarID:scholarID,
-                   password: password,
-                   profileimage: profileimage,
-                   admin:false
+                    name: name,
+                    email: email,
+                    username: username,
+                    scholarID: scholarID,
+                    password: password,
+                    profileimage: profileimage,
+                    admin: false
                 });
                 User.createUser(newUser, function (err, user) {
-                    if(err) throw err;
+                    if (err) throw err;
                     console.log(user);
                     //retrieve citations and indexes
                     retrieveCitations(scholarID, username);
                     req.flash('success', 'You are registered and can now login');
                     res.redirect('/users/login');
                 });
-            }else{
+            } else {
                 console.log(user);
                 res.render('register');
             }
@@ -112,21 +116,26 @@ router.post('/register', upload.single('profileimage'), function(req, res, next)
     }
 });
 
-router.get('/login', function(req, res, next) {
-    if(req.user)
+router.get('/login', function (req, res, next) {
+    if (req.user)
         res.redirect('/data/profile');
-    res.render('login', { title: 'Login' });
+    res.render('login', {
+        title: 'Login'
+    });
 });
 
 router.post('/login',
-    passport.authenticate('local', {failureRedirect:'/users/login', failureFlash: 'Invalid credentials'}),
-    function(req, res) {
-    req.flash('success', 'You are now logged in');
+    passport.authenticate('local', {
+        failureRedirect: '/users/login',
+        failureFlash: 'Invalid credentials'
+    }),
+    function (req, res) {
+        req.flash('success', 'You are now logged in');
 
-    res.redirect('/data/profile');
-});
+        res.redirect('/data/profile');
+    });
 
-passport.serializeUser(function(user, done){
+passport.serializeUser(function (user, done) {
     done(null, user.id);
 });
 passport.deserializeUser(function (id, done) {
@@ -137,16 +146,20 @@ passport.deserializeUser(function (id, done) {
 
 passport.use(new LocalStrategy(function (username, password, done) {
     User.getUserByUsername(username, function (err, user) {
-        if(err) throw err;
-        if(!user){
-            return done(null, false, {message: 'Unknown User'});
+        if (err) throw err;
+        if (!user) {
+            return done(null, false, {
+                message: 'Unknown User'
+            });
         }
         User.comparePassword(password, user.password, function (err, isMatch) {
-            if(err) return done(err);
-            if(isMatch){
+            if (err) return done(err);
+            if (isMatch) {
                 return done(null, user);
-            }else{
-                return done(null, false, {message:'Invalid Password'});
+            } else {
+                return done(null, false, {
+                    message: 'Invalid Password'
+                });
             }
         })
     })
